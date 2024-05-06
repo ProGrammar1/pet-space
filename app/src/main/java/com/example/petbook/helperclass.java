3292fa4f89543da402a786f45532c40aed6b9888
DarthCoder-afk <seanmichaelborje179@gmail.com>
2024-05-06 17:00:14 +0800

    eto

--- app/src/main/java/com/example/petbook/helperclass.java
+++ app/src/main/java/com/example/petbook/helperclass.java
@@ -1,14 +1,23 @@
 package com.example.petbook;
 
 public class helperclass {
-    String username, email, password,address,name;
+    String username, email, password,address,name,image;
 
-    public helperclass(String username, String email, String password, String address, String name) {
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public helperclass(String username, String email, String password, String address, String name, String image) {
         this.username = username;
         this.email = email;
         this.password = password;
         this.address = address;
         this.name = name;
+        this.image = image;
     }
 
     public String getUsername() {
